d458efe02e917535956f862f65b5b2c84e165bb3
Paolo Bursic <paolo6.bursic6@gmail.com>
2023-11-15 10:52:10 +0100

    db spojen

---
+++ src/db.js
@@ -0,0 +1,28 @@
+import mongo from "mongodb"
+
+let connnection_string = 
+"mongodb+srv://pabursic:admin@cluster0.omoamcy.mongodb.net/";
+
+let client = new mongo.MongoClient(connection_string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+export default () => {
+    return new Promise((resolve, reject) => {
+        if (db && client.isConnected()) {
+            resolve(db)
+        }
+
+        client.connect(err => {
+            if (err) {
+                reject("Doslo je do pogreske prilikom spajanja: " + err)
+            }
+            else {
+                console.log("Uspjesno spajanje na bazu")
+                db = client.db("fipogram")
+                resolve(db)
+            }
+        })
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world drugi put")
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+let brojac = 0
+
+app.get('/', (req, res) => {
+    brojac++
+
+    res.send(`Trenutna vrjednost je ${brojac}`)
+})
+
+app.listen(port, () => console.log(`Slušam na portu ${port}`)) 
