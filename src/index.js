bb5d66de7dea2f8933b3a46226b6b72a62ef197a
Paolo Bursic <paolo6.bursic6@gmail.com>
2023-12-20 12:08:11 +0100

    prva polovica endpointa za task gotova

--- src/index.js
+++ src/index.js
@@ -10,4 +10,43 @@ const port = 3000
 app.use(cors());
 app.use(express.json());
 
+//endpointi za task
+app.get('/taskovi', (req, res) => res.json(data.task));
+
+app.post('/taskovi', (req, res) => {
+    res.statusCode = 201;
+    res.setHeader('Location', '/taskovi/1234');
+    res.send();
+});
+
+app.get('/taskovi/id/:id', (req, res) => {
+    let taskId = req.params.id;
+    taskId = parseInt(taskId);
+    taskId -= 1;
+    taskId = String(taskId);
+    const task = data.task[taskId];
+
+    if (task) {
+        res.json(task);
+      } else {
+        res.status(404).send('task not found');
+      }
+});
+
+
+
+app.delete('/taskovi/:id', (req, res) => {
+  let taskId = req.params.id;
+  taskId = parseInt(taskId);
+  taskId -= 1;
+  taskId = String(taskId);
+
+    if (data.task[taskId]) {
+      //delete data.task[taskId]; 
+      res.status(204).send();
+    } else {
+      res.status(404).send('task not found');
+    }
+})
+
 app.listen(port, () => console.log(`Slušam na portu ${port}`)) 
