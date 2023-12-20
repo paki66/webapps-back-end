44349076b3541d8c8d1087c98ba8a14cdb0ca5f9
Paolo Bursic <paolo6.bursic6@gmail.com>
2023-12-20 14:32:35 +0100

    endpointi za projekt gotovi

--- src/index.js
+++ src/index.js
@@ -10,4 +10,42 @@ const port = 3000
 app.use(cors());
 app.use(express.json());
 
+// endpointi za projekte
+app.get('/projekti', (req, res) => res.json(data.projekt));
+
+app.post('/projekti', (req, res) => {
+  res.statusCode = 201;
+  res.setHeader('Location', '/projekti/1234');
+  res.send();
+});
+
+app.get('/projekti/:id', (req, res) => {
+  let projektId = req.params.id;
+  projektId = parseInt(projektId);
+  projektId -= 1;
+  projektId = String(projektId);
+  const projekt = data.projekt[projektId];
+
+  if (projekt) {
+      res.json(projekt);
+    } else {
+      res.status(404).send('project not found');
+    }
+});
+
+app.delete('/projekti/:id', (req, res) => {
+let projektId = req.params.id;
+projektId = parseInt(projektId);
+projektId -= 1;
+projektId = String(projektId);
+
+  if (data.projekt[projektId]) {
+    //delete data.izvjestaj[izvjestajId]; 
+    res.status(204).send();
+  } else {
+    res.status(404).send('Report not found');
+  }
+})
+
+
 app.listen(port, () => console.log(`Slušam na portu ${port}`)) 
