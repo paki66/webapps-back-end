de14b2b7f2012d0d008272f126c752b788fbcaf5
paolo <paolo.bursic@student.unipu.hr>
2024-03-14 14:28:38 +0100

    report endpointi gotovi

--- src/index.js
+++ src/index.js
@@ -169,46 +169,70 @@ app.patch("/tasks/expired", async (req, res) => {
 });
 
 // endpointi za m_izvjestaj
-app.get("/izvjestaji", async (req, res) => {
-  let cursor = await db.collection("izvjestaji").find();
+app.get("/reports/history", async (req, res) => {
+  let cursor = await db.collection("reports").find();
   let results = await cursor.toArray();
   res.json(results);
 });
 
-app.post("/izvjestaji", (req, res) => {
-  let dodaniIzvjestaj = req.body;
+app.get("/reports", async (req, res) => {
+  let project = req.query.project;
+  let name = req.query.name;
+  let query = {
+    $and: [
+      {"project": {$regex: new RegExp(project, 'i')}}, 
+      {"name": {$regex: new RegExp(name, 'i')}}
+    ]
+  };
+  let cursor = await db.collection("reports").find(query);
+  let results = await cursor.toArray();
+  res.json(results);
+});
+
+app.get("/reports/current", async (req, res) => {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const dateString = `${year}-${month}`;
+
+  let cursor = await db.collection("reports").find({month: {$gt: dateString}});
+  let results = await cursor.toArray();
+  res.json(results);
+});
+
+app.post("/reports", async (req, res) => {
+  let addedReport = req.body;
+  let cursor = await db.collection("reports").insertOne(addedReport);
   res.statusCode = 201;
-  res.json(dodaniIzvjestaj);
-  res.send();
+  res.json(addedReport).send;
 });
 
-app.get("/izvjestaji/:id", (req, res) => {
-  let izvjestajId = req.params.id;
-  izvjestajId = parseInt(izvjestajId);
-  izvjestajId -= 1;
-  izvjestajId = String(izvjestajId);
-  const izvjestaj = data.izvjestaj[izvjestajId];
 
-  if (izvjestaj) {
-    res.json(izvjestaj);
+
+app.delete("/reports/:project/:name", async (req, res) => {
+  let name = req.params.name;
+  let project = req.params.project;
+  if (await db.collection("reports").countDocuments({name: name, project: project}) > 0) {
+    let cursor = await db.collection("reports").deleteOne({name: name, project: project});
+    res.status(200).send("Report deleted!");
   } else {
     res.status(404).send("Report not found");
   }
 });
 
-app.delete("/izvjestaji/:id", (req, res) => {
-  let izvjestajId = req.params.id;
-  izvjestajId = parseInt(izvjestajId);
-  izvjestajId -= 1;
-  izvjestajId = String(izvjestajId);
+app.put("/reports", async (req, res) => {
+  let name = req.body.name;
+  let project = req.body.project;
+  let report = req.body;
 
-  if (data.izvjestaj[izvjestajId]) {
-    //delete data.izvjestaj[izvjestajId];
-    res.status(204).send();
-  } else {
-    res.status(404).send("Report not found");
-  }
-});
+  let put = await db.collection("reports").updateOne({name: name, project: project},
+    {$set: {month: report.month, name: report.name, project: report.project, tasks: report.tasks, user_email: report.user_email}});
+
+  let cursor = await db.collection("reports").find({name: report.name, project: report.project});
+  let result = await cursor.toArray();
+  res.json(result);
+})
 
 // endpointi za projekte
 app.get("/projects", async (req, res) => {
