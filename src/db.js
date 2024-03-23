a3ba6ccfa0fe888864e9f0c8d1465f99a89be2b7
msol0 <147321672+msol0@users.noreply.github.com>
2024-03-23 11:36:58 +0100

    popravljen path do .env

--- src/db.js
+++ src/db.js
@@ -1,9 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
-console.log(process.env);
-/*const connectionString =
-  "mongodb+srv://admin:admin@organize.ilo2skw.mongodb.net/?retryWrites=true&w=majority";*/
 const connectionString = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
