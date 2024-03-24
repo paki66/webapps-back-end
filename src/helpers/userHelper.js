eac136f737bfe2c3fe5d8a0e81c16f9d1d709da9
msol0 <147321672+msol0@users.noreply.github.com>
2024-03-24 11:56:08 +0100

    Dodan token i bcrypt.compare provjera lozinke. Ažuriran .gitignore

--- src/helpers/userHelper.js
+++ src/helpers/userHelper.js
@@ -1,26 +1,51 @@
 import db from "../db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 let usersCollection = db.collection("users");
-const signup = async (req, res) => {
+export const signup = async (req, res) => {
   console.log(req.body);
+  let passwordHash = await bcrypt.hash(req.body.password, 10);
   let user = {
     name: req.body.name,
     surname: req.body.surname,
     email: req.body.email,
-    password: req.body.password,
-    repeatPassword: req.body.repeatPassword,
+    password: passwordHash,
   };
   let result = await usersCollection.insertOne(user);
-};
-
-const login = async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await usersCollection.findOne({ email, password });
-
-  if (!user || password !== user.password) {
-    return res.status(401).json({ message: "Incorrect email or password" });
+  if (result.acknowledged === true) {
+    createToken(result, user, 201, res);
+  } else {
+    res.status(500);
   }
 };
 
-export { signup, login };
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersCollection.findOne({ email });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: "Incorrect email or password" });
+  }
+
+  createToken(user, 200, res);
+};
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+const createToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
